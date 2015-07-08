bbe16ab89bd1558afcc867840667547a8cdd2cd9
pr0v3rbs <rla5072@paran.com>
2015-07-09 00:13:18 +0900

    Remote Exploit Tool
    원격 익스플로잇 툴 업로드

---
+++ Attacker.py
@@ -0,0 +1,36 @@
+import struct
+import telnetlib
+from socket import *
+
+p8 = lambda x : struct.pack("<L", x)
+p16 = lambda x : p8(x & 0xffffffff) + p8((x & 0xffffffff00000000) >> 32)
+
+def ReadUntil(s, chkStr, isPrint = True) :
+    chkLen = len(chkStr)
+    data = s.recv(1)
+    while True :
+        while data[-1] != chkStr[0] :
+            data += s.recv(1)
+        tem = s.recv(chkLen - 1)
+        data += tem
+        # tem possible part of chkStr
+        # need to fix
+        if tem == chkStr[1:] :
+            break
+        
+    if isPrint :
+        print data
+
+HOST = "hack.me"
+PORT = 1234
+
+s = socket(AF_INET, SOCK_STREAM)
+s.connect((HOST, PORT))
+
+#exploit!
+
+t = telnetlib.Telnet()
+t.sock = s
+t.interact()
+
+s.close()
