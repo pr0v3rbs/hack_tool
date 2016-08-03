fc62da783c73461d28ad0c1d1a1e41c0c470dc4e
pr0v3rbs <rla5072@paran.com>
2016-08-04 01:33:57 +0900

    change ReadUntil function
    Improve performance and reliable by endswith function.

--- Attacker.py
+++ Attacker.py
@@ -10,12 +10,11 @@ up8 = lambda x : struct.unpack("<L", x)[0]
 up16 = lambda x : struct.unpack("<Q", x)[0]
 
 def ReadUntil(s, chkStr) :
-    chkLen = len(chkStr)
-    data = s.recv(chkLen)
-    while True :
-        if data[-1] == chkStr[-1] and data[-chkLen:] == chkStr:
-            break
-        data += s.recv(1)
+    data = s.recv(len(chkStr))
+    while not data.endswith(chkStr) :
+        tmp = s.recv(1)
+        if not tmp: break
+        data += tmp
 
     return data
 
